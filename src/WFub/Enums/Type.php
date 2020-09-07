5315fd4a4a1a333ef5f1782716beb7e43a4f9267
wnull <x4vh@yandex.ru>
2020-09-07 15:33:11 +0300

    Visual alignment

--- src/WFub/Enums/Type.php
+++ src/WFub/Enums/Type.php
@@ -4,7 +4,7 @@ namespace WFub\Enums;
 
 class Type
 {
-    public const MARK = 'mark';
-    public const BADGE = 'badge';
+    public const MARK   = 'mark';
+    public const BADGE  = 'badge';
     public const STRIPE = 'stripe';
 }
